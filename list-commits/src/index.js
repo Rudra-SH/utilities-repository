a4665477eb7247ffce2e8fc22ce7ce1d9d3b7ef3
Rudra Sharans <rudra099999@gmail.com>
2025-09-21 16:48:31 +0530

    Making javascript action

---
+++ list-commits/src/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+    const { owner, repo } = github.context.repo;
+
+    core.info('Fetching commits...');
+    
+    const commits = await octokit.paginate(
+      octokit.rest.repos.listCommits,
+      {
+        owner,
+        repo,
+        per_page: 100
+      }
+    );
+
+    core.info(`Total commits found: ${commits.length}`);
+    const commitShas = commits.map(commit => commit.sha);
+    core.setOutput('commits', JSON.stringify(commitShas));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
